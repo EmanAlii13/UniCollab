ec2cd915f03671798236231bfda21d9a57728a6a
Eman <206050@ppu.edu.ps>
2026-02-06 09:29:08 +0200

    UNC-9-feature-user-db-connection: add database.py to establish MongoDB connection

---
+++ user-service/database.py
@@ -0,0 +1,8 @@
+# uniCollab - User Service - file: database.py
+from pymongo import MongoClient
+from config import MONGO_URI, DB_NAME
+
+client = MongoClient(MONGO_URI)
+db = client[DB_NAME]
+
+users_collection = db["users"]
