a7b9e94e563913fe7654260ab7ee67b2af75be06
ayatsider <22011430@students.hebron.edu>
2026-02-08 00:04:55 +0200

    test: trigger CI

--- project-service/app/services/observer.py
+++ project-service/app/services/observer.py
@@ -1,6 +1,9 @@
+# observer_test.py
+
 class Observer:
     def notify(self, message):
         print(f"[Observer] {message}")
 
-
+# --------- TEST / CHANGE ---------
 observer = Observer()
+observer.notify("Testing CI trigger!")  # ✅ هذا السطر الجديد رح يعتبر تغيير ويشغل الـ CI
