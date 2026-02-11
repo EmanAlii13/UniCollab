eba733f39ba5c46c64cfa19ef098aa12f671a402
Eman <206050@ppu.edu.ps>
2026-02-11 21:54:39 +0200

    UNC-28-feature-user-dockerfile: add Dockerfile, docker-compose, and Nginx config for user-service

--- user-service/ models/user.py
+++ user-service/ models/user.py
@@ -9,3 +9,15 @@ class User(BaseModel):
     project_id: Optional[str] = None
     role: Optional[Literal["leader", "member", None]] = None
     last_message: Optional[str] = None
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "username": "student1",
+                "email": "student1@uni.edu",
+                "password": "hashed_password_here",
+                "project_id": None,
+                "role": None,
+                "last_message": None
+            }
+        }
